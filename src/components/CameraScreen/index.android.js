0c2a1de26fb6b2208b1383d3aa3bb3b2d9e3f6a3
Tu Phan <pmtu95@gmail.com>
2021-01-22 12:51:10 +0700

    take photo

--- src/components/CameraScreen/index.android.js
+++ src/components/CameraScreen/index.android.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import { View, PermissionsAndroid } from 'react-native';
+import { View } from 'react-native';
 import CameraScreenBase from './CameraBase';
 
 export default class CameraScreen extends CameraScreenBase {
-  componentDidMount() {
-    this.hasAndroidPermission();
-  }
-
-  hasAndroidPermission = async () => {
-    const permission = PermissionsAndroid.PERMISSIONS.CAMERA;
-
-    const hasPermission = await PermissionsAndroid.check(permission);
-    if (hasPermission) {
-      return true;
-    }
-
-    const status = await PermissionsAndroid.request(permission);
-    return status === 'granted';
-  };
-
   renderGap() {
     return <View style={{ flex: 10, flexDirection: 'column' }} />;
   }
--- src/screens/ShopCaptureScreen/ShopCaptureScreen.js
+++ src/screens/ShopCaptureScreen/ShopCaptureScreen.js
@@ -1,13 +1,31 @@
 import React, { memo } from 'react';
-import { Snackbar } from 'react-native-paper';
+
+import ImageResizer from 'react-native-image-resizer';
+import Marker, { Position } from 'react-native-image-marker';
+import moment from 'moment-timezone';
+
+import {
+  StyleSheet,
+  View,
+  NativeModules,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  Text,
+  Dimensions,
+} from 'react-native';
+import { Button, Appbar, IconButton } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
+import { defaultTheme } from '../../theme';
+
+import { savePicture } from '../../utils';
+import { objectId } from '../../utils/uniqId';
 
 // ###
-import TakePhoto from './components/TakePhoto';
-
-import * as actions from './actions';
-import * as selectors from './selectors';
 import { selectors as appSelectors, actions as appActions } from '../App';
+
+const ImagePicker = NativeModules.ImageCropPicker;
 
 const ShopCaptureScreen = ({ navigation, route }) => {
   const {
@@ -15,33 +33,184 @@ const ShopCaptureScreen = ({ navigation, route }) => {
   } = route;
 
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectors.makeSelectIsLoading());
-  const errorMessage = useSelector(selectors.makeSelectErrorMessage());
   const serverTime = useSelector(appSelectors.makeSelectServerTime());
+
+  let [photos, setPhotos] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isDeleting, setIsDeleting] = React.useState(false);
 
   React.useEffect(() => {
     dispatch(appActions.getServerTime());
   }, [dispatch]);
 
-  const onDismiss = React.useCallback(() => {
-    dispatch(actions.setError(''));
-  }, [dispatch]);
+  const onTakePhoto = () => {
+    setIsLoading(true);
+    ImagePicker.openCamera({
+      cropping: false,
+      includeExif: true,
+      mediaType: 'photo',
+    })
+      .then((image) => {
+        if (image) {
+          photos = [...photos, { ...image, localIdentifier: objectId() }];
+
+          setPhotos(photos);
+          setIsLoading(false);
+          onTakePhoto();
+        }
+      })
+      .catch(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const onRemovePhoto = (photo) => {
+    setIsDeleting(true);
+    ImagePicker.cleanSingle(photo.path)
+      .then(() => {
+        setIsDeleting(false);
+        const newPhotos = photos.filter(
+          (item) => item.localIdentifier !== photo.localIdentifier,
+        );
+        setPhotos(newPhotos);
+      })
+      .catch((e) => {
+        setIsDeleting(false);
+      });
+  };
+
+  const saveImage = () => {
+    setIsLoading(true);
+    const now = moment()
+      .tz(serverTime, 'Asia/Ho_Chi_Minh')
+      .format('HH:mm:ss DD-MM-YYYY');
+
+    if (photos.length > 0) {
+      photos.forEach((photo) => {
+        const { width, height } = Dimensions.get('window');
+        const { size, path } = photo;
+        let quality = 100;
+
+        if (size >= 200000) {
+          quality = Platform.OS === 'ios' ? 20 : 60;
+        }
+        ImageResizer.createResizedImage(path, width, height, 'JPEG', quality, 0)
+          .then((res) => {
+            Marker.markText({
+              src: res.uri,
+              color: '#FF0000',
+              fontSize: 16,
+              X: 30,
+              Y: 30,
+              scale: 1,
+              quality: 100,
+              text: `${shopName} ${now}`,
+              position: Position.topLeft,
+            })
+              .then((_path) => {
+                const uri =
+                  Platform.OS === 'android'
+                    ? 'file://' + _path
+                    : 'file:///' + _path;
+                savePicture(uri);
+                setIsLoading(false);
+              })
+              .catch(() => {
+                setIsLoading(false);
+              });
+          })
+          .catch(() => {
+            setIsLoading(false);
+          });
+
+        ImagePicker.clean().then(() => {
+          setPhotos([]);
+        });
+      });
+    } else {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
-      <TakePhoto
-        navigation={navigation}
-        shopName={shopName}
-        serverTime={serverTime}
-      />
-      <Snackbar
-        visible={!isLoading && !!errorMessage}
-        onDismiss={onDismiss}
-        duration={4000}>
-        {errorMessage}
-      </Snackbar>
+      <Appbar.Header>
+        <Appbar.BackAction
+          onPress={() => navigation.goBack()}
+          disabled={isLoading}
+        />
+        <Appbar.Content title={shopName} subtitle="" />
+      </Appbar.Header>
+      <View style={styles.container}>
+        <View style={styles.root}>
+          <View style={styles.itemBox}>
+            <IconButton
+              icon="camera"
+              size={30}
+              onPress={onTakePhoto}
+              disabled={isLoading || isDeleting}
+            />
+          </View>
+          <ScrollView horizontal>
+            {photos.map((photo) => {
+              return (
+                <View style={styles.itemBox} key={photo.localIdentifier}>
+                  <View style={[styles.item]}>
+                    <Image source={{ uri: photo.path }} style={[styles.img]} />
+                  </View>
+                  <TouchableOpacity
+                    style={[styles.btnDelete]}
+                    onPress={() => onRemovePhoto(photo)}
+                    disabled={isLoading}>
+                    <Text>Xoá</Text>
+                  </TouchableOpacity>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
+        <Button mode="contained" onPress={saveImage} disabled={isLoading}>
+          Save
+        </Button>
+      </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: defaultTheme.colors.background,
+    padding: 10,
+    flexDirection: 'column',
+  },
+  root: {
+    flexDirection: 'column',
+  },
+  itemBox: {
+    alignItems: 'center',
+  },
+  item: {
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    marginRight: 12,
+    height: 150,
+    width: 150,
+  },
+  img: {
+    borderRadius: 5,
+    resizeMode: 'cover',
+    flex: 1,
+  },
+  btnDelete: {
+    height: 40,
+    paddingTop: 20,
+    marginBottom: 20,
+    width: 150,
+    alignItems: 'center',
+  },
+});
 
 export default memo(ShopCaptureScreen);
